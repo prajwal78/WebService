9977daddc244c6183f0d3ceb07efae777c3e96bc
root <prajwal.halwai78@gmail.com>
2016-04-06 23:24:29 +0530

    final assignment code pushed

--- config/config.js
+++ config/config.js
@@ -6,13 +6,11 @@ module.exports = {
     dev: {
         name: 'Assignment API',
         startupMessage: "API Service started successfully",
-        applicationPort: 9001,
-        dbUrl: 'mongodb://abcd:abcd123@ds011820.mlab.com:11820/companydetails'
+        applicationPort: 9001
     },
     prd: {
         name: 'Assignment API',
         startupMessage: "API Service started successfully",
-        applicationPort: 9001,
-        dbUrl: 'mongodb://abcd:abcd123@ds011820.mlab.com:11820/companydetails'
+        applicationPort: 9001
     }
 };
